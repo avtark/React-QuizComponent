8a2d6e45bf4d90f701fbd0effe61f2322024c21b
A Kohli <kohli.avtar@gmail.com>
2019-01-24 16:23:41 +1100

    check my work

--- src/Quiz.js
+++ src/Quiz.js
@@ -20,8 +20,7 @@ showNextQuestion() {
     return(
       <div>
         {isQuizEnd ? <QuizEnd /> :
-        <QuizQuestion quiz_question={quizData.quiz_questions
-        [this.state.quiz_position - 1]} showNextQuestionHandler=
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler=
         {this.showNextQuestion.bind(this)} />}
       </div>
     )
